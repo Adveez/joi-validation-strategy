4a6874489fc3040dee157a42411989e23e78ab5e
Gaëtan André <g.andre@adveez.com>
2018-08-10 11:18:19 +0200

    collect data correctly for arrays

--- src/utils/collectErrors.js
+++ src/utils/collectErrors.js
@@ -2,7 +2,13 @@ import groupBy from 'lodash.groupby';
 
 export default function collectErrors(error) {
   if (error !== null) {
-    return groupBy(error.details, 'path');
+    return groupBy(error.details, detail => {
+      let res = detail.path;
+      if (Array.isArray(res) && res.length > 0) {
+        res = res[0];
+      }
+      return res;
+    });
   }
   return {};
 }
